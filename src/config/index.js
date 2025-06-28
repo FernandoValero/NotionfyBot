bc47aab46577bef85fa2b5c27e29b0772cc18961
FernandoValero <fernandovalero04@gmail.com>
2025-06-27 21:53:03 -0300

    feature/add-git-actions-feature

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,10 @@
+if (process.env.NODE_ENV !== 'production' && !process.env.GITHUB_ACTIONS) {
+    require('dotenv').config();
+}
+
 module.exports = {
     notionToken: process.env.NOTION_TOKEN,
-    databaseId: process.env.DATABASE_ID,
+    databaseId: process.env.NOTION_DATABASE_ID,
     telegramToken: process.env.TELEGRAM_TOKEN,
-    chatId: process.env.CHAT_ID
+    chatId: process.env.TELEGRAM_CHAT_ID
 };
